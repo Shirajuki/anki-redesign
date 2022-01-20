424e7ef1a1be5cdf3bc7a130d30b043c4139a131
Shirajuki <shirajuki00@gmail.com>
2022-01-20 12:43:57 +0100

    feat: updates build script

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm -f anki-redesign.ankiaddon
-zip -r anki-redesign.ankiaddon __init__.py manifest.json files/ user_files/ config.json config.md
+zip -r anki-redesign.ankiaddon files/ user_files/ __init__.py config.json config.md manifest.json utils.py
