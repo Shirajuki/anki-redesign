a9bdad380d537432218bd997564c30775b7e64ae
Shirajuki <shirajuki00@gmail.com>
2022-07-26 23:30:13 +0200

    chore: update buildscript

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+
+# Delete __pycache__
+find -name __pycache__ -type d -exec rm -rf {} +
+
+# Delete current add-on package
 rm -f anki-redesign.ankiaddon
+
+# Zip add-on (build)
 zip -r anki-redesign.ankiaddon files/ themes/ user_files/ utils/ __init__.py config.json config.md config.py manifest.json
